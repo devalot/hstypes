ae27860dec2aa42616bab2223fad41e047ba6581
Peter Jones <pjones@pmade.com>
2013-07-11 11:47:45 -0600

    Initial files for the Haskell Types presentation

---
+++ slides.el
@@ -0,0 +1,9 @@
+;;; slides.el -- Slide definitions
+;;
+;; Presentation given using Emacs and bufshow.  For more info see:
+;;
+;;   http://github.com/pjones/bufshow
+;;
+(bufshow-start
+ [("README.org"      "c4d4eaed-d7c4-4ddb-a5a6-d535603438d6")
+  ("src/intro.hs"    "intro")])
